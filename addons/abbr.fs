aadb305419e620df9d979ad23db15f4f60fc34ac
programandala.net <>
2013-07-12 22:59:00 +0000

    Update from development backup 201307130059.
    FossilOrigin-Name: 155333a4064c92cb12b979cc3f91f1e976da100c83280d811377032f7d5b4a3b

---
+++ addons/abbr.fs
@@ -0,0 +1,66 @@
+\ abbr.fs 
+
+\ This file is part of
+\ Fendo-programandala
+
+\ This file is the abbr addon.
+
+\ Copyright (C) 2013 Marcos Cruz (programandala.net)
+
+\ Fendo-programandala is free software; you can redistribute
+\ it and/or modify it under the terms of the GNU General
+\ Public License as published by the Free Software
+\ Foundation; either version 2 of the License, or (at your
+\ option) any later version.
+
+\ Fendo-programandala is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied
+\ warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+\ PURPOSE.  See the GNU General Public License for more
+\ details.
+
+\ You should have received a copy of the GNU General Public
+\ License along with this program; if not, see
+\ <http://gnu.org/licenses>.
+
+\ Fendo-programandala is written in Forth
+\ with Gforth (<http://www.bernd-paysan.de/gforth.html>).
+
+\ **************************************************************
+\ Change history of this file
+
+\ 2013-06-19 Start.
+\ 2013-06-29 All addons are defined, but fakes.
+\ 2013-07-03 Fix: '(abbr)' refills the input stream until
+\   something is found. This makes it possible to separate the
+\   addon words and the actual abbreviation in different lines.
+
+\ **************************************************************
+
+: abbr: ( "name" -- )
+  \ xxx todo
+  ;
+: (abbr)  ( "name" -- ca len )
+  \ Parse an abbreviation.
+  begin   parse-name dup 0=
+  while   2drop refill 0= abort" Missing abbr"
+  repeat
+  ;
+: abbr ( "name" -- )
+  \ xxx todo
+  (abbr) echo  \ xxx tmp
+  ;
+: abbr_meaning ( "name" -- )
+  \ xxx todo
+  (abbr) echo  \ xxx tmp
+  ;
+: abbr_translation ( "name" -- )
+  \ xxx todo
+  (abbr) echo  \ xxx tmp
+  ;
+: abbrs_list ( u -- )
+  \ xxx todo
+  \ u = heading level
+  drop \ xxx tmp
+  ;
+
