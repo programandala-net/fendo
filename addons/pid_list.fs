b2af1ab4df63eac74c702cf90a8839febf79d0df
programandala.net <>
2013-11-29 23:48:00 +0000

    Update from development backup 201311300048.
    FossilOrigin-Name: b19cf4113bd991e4c48e57766b215243bd49a7e6d3dcbad5243c4986aa0e0589

--- addons/pid_list.fs
+++ addons/pid_list.fs
@@ -1,4 +1,4 @@
-.( addons/page_id_list.fs) cr
+.( addons/pid_list.fs) cr
 
 \ This file is part of Fendo.
 
@@ -30,41 +30,67 @@
 \ <addons/list_of_content_by_prefix.fs>.
 \ 2013-11-26 Change: several words renamed, after a new uniform
 \   notation: "pid$" and "pid#" for both types of page ids.
+\ 2013-11-27 Change: all words and the addon itself renamed.
+\ 2013-11-27 New: the list doesn't include file paths.
+\ 2013-11-27 Change: '(pid$_list@)' factored out from 'pid$_list@'.
+\ 2013-11-27 New: 'pid$_list@' rewritten: now it skips draft pages.
 
 \ **************************************************************
 
 require galope/module.fs  \ 'module:', ';module', 'hide', 'export'
 require galope/slash-counted-string.fs  \ '/counted-string'
 
-module: page_id_list_fendo_addon_module
+module: pid_list_fendo_addon_module
 
-s" /tmp/list_of_content_common_fendo_addon.txt" 2constant pages_list_file$
-: create_pages_list  ( -- )
+s" /tmp/pid_list_fendo_addon.txt" 2constant pid_list_file$
+: ls_command$  ( -- ca len )
   s" ls -1 " source_dir $@ s+ s" *" s+ forth_extension $@ s+
-  s"  > " s+ pages_list_file$ s+ system
   ;
-0 value pages_list_fid
-/counted-string 2 + buffer: pages_list_element
+: +ls_filter  ( ca1 len1 -- ca2 len2 )
+  s" |sed -e 's#.\+/\(.\+\)\" s+ forth_extension $@ s+ s" #\1#'" s+
+  ;
+: +sort_filter  ( ca1 len1 -- ca2 len2 )
+  s" |sort" s+
+  ;
+: pid_list_command$  ( -- ca len )
+  ls_command$ +ls_filter +sort_filter
+  ;
+: create_pid_list  ( -- )
+  pid_list_command$
+\  2dup type cr  \ xxx informer
+  s"  > " s+ pid_list_file$ s+ system
+  ;
+0 value pid_list_fid
+/counted-string 2 + buffer: pid_list_element
+: (pid$_list@)  ( -- ca len wf )
+  \ Return the next page id from the list.
+  \ ca len = page id
+  \ wf = is the page id valid? (=is the list not empty?)
+  pid_list_element dup /counted-string
+  pid_list_fid read-line throw
+  ;
 
 export
 
-: open_page_id_list  ( -- )
-  create_pages_list
-  pages_list_file$ r/o open-file throw
-  to pages_list_fid
+: open_pid_list  ( -- )
+  create_pid_list
+  pid_list_file$ r/o open-file throw
+  to pid_list_fid
   ;
-: close_page_id_list  ( -- )
-  pages_list_fid close-file throw
+: close_pid_list  ( -- )
+  pid_list_fid close-file throw
   ;
-: page_id_list@  ( -- ca len )
-  \ Return the next page id from the pages list,
+: pid$_list@  ( -- ca len )
+  \ Return the next page id from the list,
   \ or an empty string if the list is empty.
-  pages_list_element dup /counted-string
-  pages_list_fid 
-  read-line throw and  source>pid$
+  0.  \ for the first 2drop
+  begin   2drop (pid$_list@)
+    if    2dup (pid$>data>pid#) draft? 0=
+    else  true  then
+  until
   ;
 
 ;module
 
-.( addons/list_of_content_by_prefix.fs compiled) cr
+.( addons/pid_list.fs compiled) cr
 
