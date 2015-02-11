bd3b9318804b6bc3b6a08681bd5f7d3fdd70caff
programandala.net <>
2015-02-11 19:04:00 +0000

    Update from development backup 201502112104.
    FossilOrigin-Name: 412886d5fe32eb23c626d2f509736e1f29d814d2946bd2eee45b012ad83f81b5

---
+++ fendo.addon.XXX.fs
@@ -0,0 +1,41 @@
+.( fendo.addon.XXX.fs ) cr
+
+\ This file is part of Fendo.
+
+\ This file is the XXX addon.
+\ It ...
+
+\ Copyright (C) 2015 Marcos Cruz (programandala.net)
+
+\ Fendo is free software; you can redistribute
+\ it and/or modify it under the terms of the GNU General
+\ Public License as published by the Free Software
+\ Foundation; either version 2 of the License, or (at your
+\ option) any later version.
+
+\ Fendo is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied
+\ warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+\ PURPOSE.  See the GNU General Public License for more
+\ details.
+
+\ You should have received a copy of the GNU General Public
+\ License along with this program; if not, see
+\ <http://gnu.org/licenses>.
+
+\ **************************************************************
+\ Change history of this file
+
+\ See at the end of the file.
+
+\ **************************************************************
+\ Main
+
+
+\ **************************************************************
+\ Change history of this file
+
+\ 2015-xx-xx: 
+
+.( fendo.addon.XXX.fs compiled ) cr
+
