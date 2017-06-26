dee984254088e59704ee77394aa5b4bc620403f6
Marcos Cruz <mercurial-repo@programandala.net>
2017-06-26 14:37:43 +0000

    Move version to its own file; update versions doc.
    FossilOrigin-Name: d50d83e3929c268e38fdfe760fd2e6401a8a0a67a382f75133c19ec1c34f59dc

---
+++ VERSION.fs
@@ -0,0 +1,9 @@
+.( VERSION.fs ) cr
+
+\ This file is part of Fendo
+\ (http://programandala.net/en.program.fendo.html).
+
+: fendo_version ( -- ca len ) s" 0.6.0-pre.5+201706261624" ;
+
+\ vim: filetype=gforth
+
--- fendo.fs
+++ fendo.fs
@@ -5,7 +5,7 @@
 
 \ This file is the main one; it loads all the modules.
 
-\ Last modified 201706251727.
+\ Last modified 201706261624.
 \ See change log at the end of the file.
 
 \ Copyright (C) 2012,2013,2014,2015,2017 Marcos Cruz (programandala.net)
@@ -107,8 +107,8 @@ false [if]
 \ Other 
 
 : empty? ( ca len -- f )
-  \ Is a string empty?
   nip 0= ;
+  \ Is a string empty?
 
 \ ==============================================================
 
@@ -217,7 +217,7 @@ fendo_definitions
 \ ==============================================================
 \ Config
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.4+201706260129" ;
+require VERSION.fs
 
 : generator ( -- ca len )
   s" Fendo (Forth Engine for Net DOcuments) " fendo_version s+ ;
