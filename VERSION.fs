15e9cda6d58f1105420c2c01984dc216193a392c
programandala.net <>
2017-11-15 19:46:03 +0000

    Replace Galope's deprecated `++` with `1+!`.
    FossilOrigin-Name: 07adae9ccc891af4ca9dc65686e86d7578e515c4c7dc68a5db226c034a42c930

--- VERSION.fs
+++ VERSION.fs
@@ -3,7 +3,7 @@
 \ This file is part of Fendo
 \ (http://programandala.net/en.program.fendo.html).
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.7+201711041743" ;
+: fendo_version ( -- ca len ) s" 0.6.0-pre.8+201711152045" ;
 
 \ vim: filetype=gforth
 
