f471d00b085017cc290690decb3f309c918302eb
programandala.net <>
2018-08-20 11:58:31 +0000

    Deactivate "--compact" in `include_asciidoctor`.
    FossilOrigin-Name: be41f2d8a8dcdc833a2d6b0c97474ca6bc8ae40ab2708f6dce38494ce252a935

--- VERSION.fs
+++ VERSION.fs
@@ -3,7 +3,7 @@
 \ This file is part of Fendo
 \ (http://programandala.net/en.program.fendo.html).
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.9+201712042307" ;
+: fendo_version ( -- ca len ) s" 0.6.0-pre.10+201808201357" ;
 
 \ vim: filetype=gforth
 
