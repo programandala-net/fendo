846453b17f7a8d9db038c7a4b33ca926060b58e4
Marcos Cruz <mercurial-repo@programandala.net>
2018-09-27 15:50:12 +0000

    Improve local variable name and documentation.
    FossilOrigin-Name: 0acdf5532564fbeb1a0003a1862a25c7d37b4352f5a0e5b7a04d7c741424b600

--- VERSION.fs
+++ VERSION.fs
@@ -3,7 +3,7 @@
 \ This file is part of Fendo
 \ (http://programandala.net/en.program.fendo.html).
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.12+201809271543" ;
+: fendo_version ( -- ca len ) s" 0.6.0-pre.13+201809271746" ;
 
 \ vim: filetype=gforth
 
