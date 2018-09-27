78908c251be4095fe83249edf7501e19561b61ac
programandala.net <>
2018-09-27 15:52:09 +0000

    Add `brother_pages?`.
    FossilOrigin-Name: 654657c97b22123610f71a8f32c9c221b40e680a85c9ea2196c12d576b8278fb

--- VERSION.fs
+++ VERSION.fs
@@ -3,7 +3,7 @@
 \ This file is part of Fendo
 \ (http://programandala.net/en.program.fendo.html).
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.13+201809271746" ;
+: fendo_version ( -- ca len ) s" 0.6.0-pre.14+201809271750" ;
 
 \ vim: filetype=gforth
 
