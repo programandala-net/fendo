7e765bbd61e0ad0993afce6bac3e0fd69661bd0d
programandala.net <>
2018-09-28 08:52:17 +0000

    Add `?previous_page`, `?next_page`.
    FossilOrigin-Name: 293e87da46157721ed48d926963a2f8aefe738d2d0d73ad5ca46d8afa622354f

--- VERSION.fs
+++ VERSION.fs
@@ -3,7 +3,7 @@
 \ This file is part of Fendo
 \ (http://programandala.net/en.program.fendo.html).
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.15+201809271801" ;
+: fendo_version ( -- ca len ) s" 0.6.0-pre.17+201809281050" ;
 
 \ vim: filetype=gforth
 
