e1da1c9e83d1a52a7f221f40bdfe9910bb4bc134
programandala.net <>
2018-12-20 18:16:56 +0000

    Add metadata `published`, `menu_title`, `breadcrum_title`. Formerly they were defined by the application.
    FossilOrigin-Name: b265a4afafeebfc9026a3577f008da99cc797b9a211744d5b4f1c08d17e4c225

--- VERSION.fs
+++ VERSION.fs
@@ -3,7 +3,7 @@
 \ This file is part of Fendo
 \ (http://programandala.net/en.program.fendo.html).
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.28+201812191515" ;
+: fendo_version ( -- ca len ) s" 0.6.0-pre.29+201812201913" ;
 
 \ vim: filetype=gforth
 
