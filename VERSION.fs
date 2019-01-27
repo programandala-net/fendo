08da6894dbb46f1578b34c11bd43f1fb67a06ddf
programandala.net <>
2019-01-27 18:49:28 +0000

    Fix `include_asciidoctor`.
    FossilOrigin-Name: 3844c1f016bd82d930c81a2a39d90211684e77426590e307728e9bbc90096e4b

--- VERSION.fs
+++ VERSION.fs
@@ -3,7 +3,7 @@
 \ This file is part of Fendo
 \ (http://programandala.net/en.program.fendo.html).
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.36+201901192005" ;
+: fendo_version ( -- ca len ) s" 0.6.0-pre.37+201901271949" ;
 
 \ vim: filetype=gforth
 
