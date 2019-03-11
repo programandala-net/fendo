a144c1993b63f7fba7f513bc5b6145604a61d11d
programandala.net <>
2019-03-11 22:21:57 +0000

    Fix `translation,` and `(translation)`. The first version stored empty dynamic strings by default and used them to check the unset translations. This made it impossible to use empty strings as actual translations, which sometimes is useful. The fixed version stores zeros instead and replaces them with the address of a dynamic string only when the translation is provided.
    FossilOrigin-Name: 579c95de903a2bb1dac490756d4c94a7a842f1b5de21d83a2f60839341f73c99

--- VERSION.fs
+++ VERSION.fs
@@ -3,7 +3,7 @@
 \ This file is part of Fendo
 \ (http://programandala.net/en.program.fendo.html).
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.40+201903112235" ;
+: fendo_version ( -- ca len ) s" 0.6.0-pre.41+201903112319" ;
 
 \ vim: filetype=gforth
 
