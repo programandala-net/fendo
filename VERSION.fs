e4d1865285a7eb114812d69854b47ca19412655c
programandala.net <>
2019-03-11 18:10:45 +0000

    Improve `empty_stack`.
    FossilOrigin-Name: acda2a76216f327c5b97b7cb535a1fed80636d6a4e65762c8126570f0ce7aa3a

--- VERSION.fs
+++ VERSION.fs
@@ -3,7 +3,7 @@
 \ This file is part of Fendo
 \ (http://programandala.net/en.program.fendo.html).
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.38+201902070010" ;
+: fendo_version ( -- ca len ) s" 0.6.0-pre.39+201903111909" ;
 
 \ vim: filetype=gforth
 
