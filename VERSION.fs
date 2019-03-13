4699ed8a0284ad405246079f7631865dc77fa482
programandala.net <>
2019-03-13 23:56:19 +0000

    Move `begin-translation` to Galope. It is usable by other applications.
    FossilOrigin-Name: 57649988db43109fd8c6fa9adb9acfc77251941d5be3ffb234269cad972584bf

--- VERSION.fs
+++ VERSION.fs
@@ -3,7 +3,7 @@
 \ This file is part of Fendo
 \ (http://programandala.net/en.program.fendo.html).
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.41+201903112319" ;
+: fendo_version ( -- ca len ) s" 0.6.0-pre.42+201903140052" ;
 
 \ vim: filetype=gforth
 
