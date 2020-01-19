ff06a9449fd7ae10ee00f51699f1041d90ed9d31
Marcos Cruz <mercurial-repo@programandala.net>
2020-01-19 00:52:54 +0000

    Use Galope's stringer, `ss"` and `ss+`.
    FossilOrigin-Name: a28b8e851a0987d3342d163e197d1f4d3755e68a33dcc3d6fa2e5a4aa80d9b34

--- VERSION.fs
+++ VERSION.fs
@@ -3,7 +3,7 @@
 \ This file is part of Fendo
 \ (http://programandala.net/en.program.fendo.html).
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.44+201907312259" ;
+: fendo_version ( -- ca len ) s" 0.6.0-pre.45+202001190149" ;
 
 \ vim: filetype=gforth
 
