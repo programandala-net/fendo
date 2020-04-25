524d7ec3aac396c42c088c5751280c8c7905eea7
Marcos Cruz <mercurial-repo@programandala.net>
2020-04-25 21:14:28 +0000

    Fix/improve `new_redirect` and `new_redirected`.
    FossilOrigin-Name: 0497990925414569ae56fbc094ab89ad26581b875a5cea28dd8b9797e42d30bc

--- VERSION.fs
+++ VERSION.fs
@@ -3,7 +3,7 @@
 \ This file is part of Fendo
 \ (http://programandala.net/en.program.fendo.html).
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.50+202004220006" ;
+: fendo_version ( -- ca len ) s" 0.6.0-pre.51+202004252314" ;
 
 \ vim: filetype=gforth
 
