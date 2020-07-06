f3f55dc7e23e91635df7af261ced878daa5477de
programandala.net <>
2020-07-06 19:39:33 +0000

    Document `tag_section` and `tag_section_by_prefix`.
    FossilOrigin-Name: e16c999891e37bdd8eea93e45c45914dfb63f498bf03ba22ab3d692db29c6329

--- VERSION.fs
+++ VERSION.fs
@@ -3,7 +3,7 @@
 \ This file is part of Fendo
 \ (http://programandala.net/en.program.fendo.html).
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.53+202007060456" ;
+: fendo_version ( -- ca len ) s" 0.6.0-pre.54+202007062137" ;
 
 \ vim: filetype=gforth
 
