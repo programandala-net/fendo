345f1e9fc9cd4585bd1a64c8a614bde57f8f7f46
programandala.net <>
2020-10-06 23:38:22 +0000

    Update version.
    FossilOrigin-Name: cbe371faaa809f62cc60465a02a4fee9c8cd435a80eb8587ff2f9f3cf9ce6250

--- VERSION.fs
+++ VERSION.fs
@@ -3,7 +3,7 @@
 \ This file is part of Fendo
 \ (http://programandala.net/en.program.fendo.html).
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.54+202007062137" ;
+: fendo_version ( -- ca len ) s" 0.6.0-pre.55+202010070138" ;
 
 \ vim: filetype=gforth
 
