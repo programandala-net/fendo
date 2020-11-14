0bb649cbf391d8596566bc7c8106dcf7d7e9e5a9
programandala.net <>
2020-11-14 23:45:00 +0000

    Split the images code into generic and markup files.
    FossilOrigin-Name: 4c7b08f91306a3588b5f0714576ccfe57ffd73a94b96073443d611146b7a7a89

--- fendo.markup.fendo.image.fs
+++ fendo.markup.fendo.image.fs
@@ -29,52 +29,16 @@
 \ ==============================================================
 \ Requirements
 
-forth_definitions
+fendo_definitions
 
-require galope/jpeg.fs  \ JPEG tools
-require galope/png.fs  \ PNG tools
+require fendo.image.fs
 
 \ ==============================================================
-\ Tools
-
-fendo_definitions
+\ Tools to parse the image markup
 
 : get_image_src_attribute ( "name" -- )
   files_subdir $@ parse-name s+ src=! ;
   \ Parse and store the image src attribute.
-
-defer img-open
-defer img-size
-defer img-close
-
-: set_jpeg_image ( -- )
-  ['] jpeg-open is img-open
-  ['] jpeg-size is img-size
-  ['] jpeg-close is img-close ;
-
-: set_png_image ( -- )
-  ['] png-open is img-open
-  ['] png-size is img-size
-  ['] png-close is img-close ;
-
-: jpeg-filename? ( ca len -- f )
-  2dup s" .jpg" string-suffix? >r
-  s" .jpeg" string-suffix? r> or ;
-
-: png-filename? ( ca len -- f )
-  s" .png" string-suffix? ;
-
-: set_image_type ( ca len -- )
-  2dup jpeg-filename? if  2drop set_jpeg_image exit  then
-  png-filename? if  set_png_image  exit  then
-  true abort" Unknown image file type." ;
-
-: (set_image_size_attributes) ( ca len -- )
-  2dup set_image_type img-open img-size
-  n>str height=! n>str width=! img-close ;
-
-: set_image_size_attributes ( -- )
-  target_dir $@ src=@ s+ (set_image_size_attributes) ;
 
 variable image_finished?  \ flag, no more image markup to parse?
 
@@ -163,39 +127,6 @@ markup_definitions
   \
   \ }doc
 
-fendo_definitions
-
-: img ( ca1 len1 ca2 len2 -- )
-  alt=!
-  files_subdir $@ 2swap s+ 2dup src=!
-  target_dir $@ 2swap s+ (set_image_size_attributes)
-  [<img>] ;
-
-  \ doc{
-  \
-  \ img ( ca1 len1 ca2 len2 -- )
-  \
-  \ Define an image with source file _ca1 len1_ and alt text _ca2
-  \ len2_. ``img`` is a direct Fendo word, not a Fendo markup word.
-  \ Therefore it must be used between `<[` and `]>`. See `{{` for an
-  \ image markup.
-  \
-  \ Usage examples:
-
-  \ ----
-  \ <[ mypicture.jpg img ]>
-  \ <[ "there/another.jpg" "Optional alternative text here" img ]>
-  \ <[ "over/there/this_one.jpg" "Alt text" title=" Fendo picture" img ]>
-  \ <[ "over/there/this_one.jpg" "" title=" Fendo picture" img ]>
-  \ ----
-
-  \ - The size attributes of the images are added automatically.
-  \ - Only JPEG and PNG images are supported.
-  \
-  \ See also: `{{`.
-  \
-  \ }doc
-
 .( fendo.markup.fendo.image.fs compiled ) cr
 
 \ ==============================================================
@@ -214,6 +145,7 @@ fendo_definitions
 \ 2020-10-09: Fix typo in stack comment of `parse_image`.
 \
 \ 2020-11-14: Remove old unused code from `parse_image`. Add `img`.
-\ Factor `set_image_size_attributes`.
+\ Factor `set_image_size_attributes`. Move the general code to a new
+\ file <fendo.image.fs>, keep here the markup-specific code.
 
 \ vim: filetype=gforth
