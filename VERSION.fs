02ba544a0a8c4876780dc8205c54618254216708
Marcos Cruz <mercurial-repo@programandala.net>
2020-11-14 22:13:17 +0000

    Fix and improve documentation of `link`.
    FossilOrigin-Name: 8d22345e67e8449680c8eb57ab6694f5e8400b313bf1e4ab978fc24ff94cea0b

--- VERSION.fs
+++ VERSION.fs
@@ -3,7 +3,7 @@
 \ This file is part of Fendo
 \ (http://programandala.net/en.program.fendo.html).
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.63+202011142027" ;
+: fendo_version ( -- ca len ) s" 0.6.0-pre.64+202011142312" ;
 
 \ vim: filetype=gforth
 
