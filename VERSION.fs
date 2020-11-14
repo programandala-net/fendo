c3910c630d1ec57c14509721056e35834aca8aea
Marcos Cruz <mercurial-repo@programandala.net>
2020-11-14 19:20:03 +0000

    Document `link`.
    FossilOrigin-Name: 7a7cbf29b4c1521deefa436d620f98957fc3cd02ba23c02d1c5a6090d0b7feaf

--- VERSION.fs
+++ VERSION.fs
@@ -3,7 +3,7 @@
 \ This file is part of Fendo
 \ (http://programandala.net/en.program.fendo.html).
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.60+202010141758" ;
+: fendo_version ( -- ca len ) s" 0.6.0-pre.61+202011142019" ;
 
 \ vim: filetype=gforth
 
