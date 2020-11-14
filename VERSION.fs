48699bcf9e33a1127e223ea131645ce82a5b2056
programandala.net <>
2020-11-14 19:28:41 +0000

    Tidy strings parsed by parsing HTML attributes.
    FossilOrigin-Name: 72bf5479953c5560b116af123961af8584e1ff96193c5ee864736987d91c7c5c

--- VERSION.fs
+++ VERSION.fs
@@ -3,7 +3,7 @@
 \ This file is part of Fendo
 \ (http://programandala.net/en.program.fendo.html).
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.62+202011142021" ;
+: fendo_version ( -- ca len ) s" 0.6.0-pre.63+202011142027" ;
 
 \ vim: filetype=gforth
 
