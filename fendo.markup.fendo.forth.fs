f63fc592b2e70cd152a9f83fa5e7019d596b25d7
programandala.net <>
2020-11-15 00:24:13 +0000

    Tidy the `<[` module and remove old code.
    FossilOrigin-Name: 904d98514784a04e05d9a60d7abe0fd55941b4bfb5d3bbd35e48f8daa4951eb1

--- fendo.markup.fendo.forth.fs
+++ fendo.markup.fendo.forth.fs
@@ -5,7 +5,7 @@
 
 \ This file defines the Fendo markup for inline Forth code.
 
-\ Last modified 202011142326.
+\ Last modified 202011150119.
 \ See change log at the end of the file.
 
 \ Copyright (C) 2013,2014,2017,2018,2020 Marcos Cruz (programandala.net)
@@ -40,115 +40,41 @@ require galope/dollar-variable.fs  \ `$variable`
 
 fendo_definitions
 
-0 [if]  \ XXX first version
+: forth-start? ( ca len - f )
+  s" <[" str= ; 
 
-: (forth_code_end?) ( ca len -- f )
-\  ." «" 2dup type ." » "  \ XXX INFORMER
-\  2dup type space \ XXX INFORMER
-  2dup s" <[" str= abs forth_code_depth +!
-       s" ]>" str= dup forth_code_depth +!
-  forth_code_depth @
-\  dup ." {" . ." }" \ XXX INFORMER
-  0= and
-\  dup  if ." END!" then  \ XXX INFORMER
-\  key drop  \ XXX INFORMER
-  ;
-  \ Is a name a valid end markup of the Forth code?
-  \ ca len = latest name parsed
-
-: forth_code_end? ( ca1 len1 ca2 len2 -- ca1' len1' f )
-  2dup (forth_code_end?) dup >r
-  0= and  \ empty the name if it's the end of the code
-  s+ s"  " s+  r> ;
-  \ Add a new name to the parsed merged Forth code
-  \ and check if it's the end of the Forth code.
-  \ ca1 len1 = code already parsed
-  \ ca1' len1' = code already parsed, with ca2 len2 added
-  \ ca2 len2 = latest name parsed
-  \ f = is ca2 len2 the right markup for the end of the code?
-
-: parse_forth_code ( "forthcode ]>" -- ca len )
-  s" "
-  begin   parse-name dup
-    if
-\          2dup ." { " type ." } "  \ XXX INFORMER
-          2dup forth_code_end?
-          dup >r
-          0= and  s+ s"  " s+ r>
-    else  2drop
-          \ s\" \n" s+
-          s"  " s+
-          refill 0=
-    then
-  until
-\  cr ." <[ " 2dup type ." ]>" cr  \ XXX INFORMER
-  ;
-  \ Get the content of a merged Forth code.
-  \ Parse the input stream until a valid "]>" markup is found.
-  \ ca len = Forth code
-
-[then]
-
-true [if]  \ XXX 2013-08-10: second version, more legible
-
-: "<["= ( -- f )
-  s" <[" str= ;
-
-: "]>"= ( -- f )
-  s" ]>" str= ;
+: forth-end? ( ca len - f )
+  s" ]>" str= ; 
 
 : update_forth_code_depth ( ca len -- )
-  2dup "<["= abs >r "]>"= r> + forth_code_depth +! ;
+  2dup forth-start? abs >r
+       forth-end?       r> + forth_code_depth +! ;
   \ ca len = latest name parsed
 
-: forth_code_end? ( ca len -- f )
-  "]>"= forth_code_depth @ 0= and ;
+: last_forth_end? ( ca len -- f )
+  forth-end? forth_code_depth @ 0= and ;
 
 : bl+ ( ca len -- ca' len' )
   s"  " s+ ;
 
-: remaining  ( -- )
-  >in @ source 2 pick - -rot + swap
-  64 min
-  cr ." ***> " type ."  <***" cr ;
-  \ XXX INFORMER
-
 : parse_forth_code ( "forthcode ]>" -- ca len )
   s" "
-  begin   parse-name dup
-    if
-\           2dup ." { " type ." } "  \ XXX INFORMER
-\           ." { " input-lexeme 2@ type ." } "  \ XXX INFORMER
-\           remaining  key drop  \ XXX INFORMER
-          2dup update_forth_code_depth
-          2dup forth_code_end?
-          dup >r if  2drop  else
-\          2dup ." {{ " type ." }}"  \ XXX INFORMER
-          s+ bl+  then  r>
-    else  2drop bl+  refill 0=  then
-  until
-\  ." ]>" cr  \ XXX INFORMER
-\  cr ." <[ " 2dup type ." ]>" cr  \ XXX INFORMER
-  ;
+  begin  parse-name dup
+    if   2dup update_forth_code_depth
+         2dup last_forth_end?
+         dup >r if 2drop else s+ bl+ then r>
+    else 2drop bl+ refill 0= then
+  until ;
   \ Get the content of a merged Forth code.
   \ Parse the input stream until a valid "]>" markup is found.
   \ ca len = Forth code
 
-[then]
-
-0 [if]  \ experimental version with dynamic string, not finished
+0 [if]  \ XXX TODO experimental version with a dynamic string
 
 : forth_code_end? ( ca len -- f )
-\  ." «" 2dup type ." » "  \ XXX INFORMER
-\  2dup type space \ XXX INFORMER
   2dup s" <[" str= abs forth_code_depth +!
        s" ]>" str= dup forth_code_depth +!
-  forth_code_depth @
-\  dup ." {" . ." }" \ XXX INFORMER
-  0= and
-\  dup  if ." END!" then  \ XXX INFORMER
-\  key drop  \ XXX INFORMER
-  ;
+  forth_code_depth @ 0= and ;
   \ Is a name a valid end markup of the Forth code?
   \ ca len = latest name parsed
 
@@ -176,11 +102,8 @@ $variable forth_code$
 
 : evaluate_forth_code ( i*x ca len -- j*x )
   get-order n>r
-  only fendo>order markup>order forth>order
-  evaluate
-  nr> set-order
-\  cr ." <[..]> done!" key drop  \ XXX INFORMER
-  ;
+  only fendo>order markup>order forth>order evaluate
+  nr> set-order ;
 
 \ ==============================================================
 \ Markup
@@ -189,22 +112,13 @@ markup_definitions
 
 : <[ ( "forthcode ]>" -- )
   1 forth_code_depth +!
-  parse_forth_code
-\  cr ." <[ " 2dup type ." ]>" cr key drop \ XXX INFORMER
-  evaluate_forth_code ; immediate
+  parse_forth_code evaluate_forth_code ; immediate
   \ Start, parse and interpret a Forth block.
 
 : ]> ( -- )
-  forth_code_depth @
-\  dup   \ XXX
-  0= abort" `]>` without `<[`"
-\  1 = if  \ XXX
-\    only markup>order
-\    separate? off
-\  then
+  forth_code_depth @ 0= abort" `]>` without `<[`"
   -1 forth_code_depth +! ; immediate
   \ Finish a Forth block.
-  \ XXX TODO
 
 fendo_definitions
 
@@ -222,5 +136,7 @@ fendo_definitions
 \ 2020-04-14: Remove old `>sb`.
 \
 \ 2020-11-14: Delete old useless compilation condition.
+\
+\ 2020-11-15: Delete old unused code and check points.
 
 \ vim: filetype=gforth
