1f8c175df164da7573b043a4f35f29c35fa97bd2
programandala.net <>
2020-11-15 19:43:41 +0000

    Document `<[` and `]>`.
    FossilOrigin-Name: e227565db604d002f2b5e851e7f45c250574f2bd46f24d1b46b272e048f02df3

--- fendo.markup.fendo.forth.fs
+++ fendo.markup.fendo.forth.fs
@@ -5,7 +5,7 @@
 
 \ This file defines the Fendo markup for inline Forth code.
 
-\ Last modified 202011150134.
+\ Last modified 202011151801.
 \ See change log at the end of the file.
 
 \ Copyright (C) 2013,2014,2017,2018,2020 Marcos Cruz (programandala.net)
@@ -55,7 +55,7 @@ fendo_definitions
 : bl+ ( ca len -- ca' len' )
   s"  " s+ ;
 
-: parse_forth_code ( "forthcode ]>" -- ca len )
+: parse_forth_code ( "ccc" -- ca len )
   s" "
   begin  parse-name dup
     if   2dup forth_code_end?
@@ -64,7 +64,6 @@ fendo_definitions
   until ;
   \ Get the content of a merged Forth code.
   \ Parse the input stream until a valid "]>" markup is found.
-  \ ca len = Forth code
 
 : evaluate_forth_code ( i*x ca len -- j*x )
   get-order n>r
@@ -76,15 +75,45 @@ fendo_definitions
 
 markup_definitions
 
-: <[ ( "forthcode ]>" -- )
+: <[ ( "ccc" -- )
   1 forth_code_depth +!
   parse_forth_code evaluate_forth_code ; immediate
-  \ Start, parse and interpret a Forth block.
+
+  \ doc{
+  \
+  \ <[ ( "ccc" -- )
+  \
+  \ Start a Forth code block, finished by `]>`. Parse the input stream
+  \ until a valid (unnested) `]>` is found, then evaluate the parsed
+  \ string as Forth code.
+  \
+  \ For convenience, Forth blocks delimited by the marks ``<[`` and
+  \ `]>` can be nested. Nesting doesn't makes any difference in the
+  \ execution of the code: The whole code delimited by the outer marks
+  \ is evaluated as if the inner ones didn't exist.
+  \
+  \ Usage example:
+
+  \ ----
+  \ _ This is a paragraph that contains
+  \ <[ s" Forth code!" echo ]> .
+  \ ----
+
+  \ }doc
 
 : ]> ( -- )
   forth_code_depth @ 0= abort" `]>` without `<[`"
   -1 forth_code_depth +! ; immediate
-  \ Finish a Forth block.
+
+  \ doc{
+  \
+  \ ]> ( -- )
+  \
+  \ Mark the end of a Forth code block, started by `<[`.
+  \
+  \ See `<[` for a usage example.
+  \
+  \ }doc
 
 fendo_definitions
 
@@ -104,6 +133,6 @@ fendo_definitions
 \ 2020-11-14: Delete old useless compilation condition.
 \
 \ 2020-11-15: Delete old unused code and check points. Simplify
-\ `parse_forth_code`.
+\ `parse_forth_code`. Document `<[` and `]>`.
 
 \ vim: filetype=gforth
