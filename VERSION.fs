85d5411ad123c8e4dc1a8401537a177e59ef6c3b
programandala.net <>
2020-11-28 21:17:09 +0000

    Fix documentation on Fendo markup and escaping of tildes.
    FossilOrigin-Name: 9fe1bfc5ac22a8b883d3937137604fbc997eaf5d0fff4729d2b22ab457d6d6e9

--- VERSION.fs
+++ VERSION.fs
@@ -3,6 +3,6 @@
 \ This file is part of Fendo
 \ (http://programandala.net/en.program.fendo.html).
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.77+20201128" ;
+: fendo_version ( -- ca len ) s" 0.6.0-pre.78+20201128" ;
 
 \ vim: filetype=gforth
