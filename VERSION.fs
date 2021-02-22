dcd64654a4b476f88681872664b510bbe0e41648
programandala.net <>
2021-02-22 23:08:58 +0000

    Make word names and factoring clearer in the <fendo.markup.fendo.passthrough.fs> module.
    FossilOrigin-Name: 1dedb6cf40d1c99ce02c5354e06d2ec5553a8d697676ab7578d081131fe93450

--- VERSION.fs
+++ VERSION.fs
@@ -3,6 +3,6 @@
 \ This file is part of Fendo
 \ (http://programandala.net/en.program.fendo.html).
 
-: fendo_version ( -- ca len ) s" 0.6.0-pre.78+20201128" ;
+: fendo_version ( -- ca len ) s" 0.6.0-pre.79+20210223" ;
 
 \ vim: filetype=gforth
